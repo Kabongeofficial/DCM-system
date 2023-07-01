4c194cf75a514ebff079bf8035e167e7bd64a0cb
Kabongeofficial <musakabonge01@gmail.com>
2023-07-01 15:24:09 +0300

    more functionality

--- admin/userprofile.php
+++ admin/userprofile.php
@@ -2,20 +2,20 @@
 session_start();
 include('include/config.php');
 if(strlen($_SESSION['alogin'])==0)
-{ 
-    header('location:index.php');
+  { 
+header('location:index.php');
 }
 else{
 
-?>
+ ?>
 <script language="javascript" type="text/javascript">
 function f2()
 {
-    window.close();
-}
+window.close();
+}ser
 function f3()
 {
-    window.print(); 
+window.print(); 
 }
 </script>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -33,13 +33,20 @@ function f3()
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 <?php 
 
-$ret1=mysqli_query($bd,"select * FROM users where id='.$_GET['uid'].'");
+$ret1=mysqli_query($bd, "select * FROM users where id='".$_GET['uid']."'");
 while($row=mysqli_fetch_array($ret1))
 {
 ?>
+
+    
+  
+		
     <tr>
-      <td colspan="2"><b>Profile</b></td>
+      <td colspan="2"><b><?php echo $row['fullName'];?>'s profile</b></td>
+      
     </tr>
+    
+    
     <tr>
       <td  >&nbsp;</td>
       <td >&nbsp;</td>
@@ -52,31 +59,45 @@ while($row=mysqli_fetch_array($ret1))
       <td><b>User Email:</b></td>
       <td><?php echo htmlentities($row['userEmail']); ?></td>
     </tr>
-    <tr height="50">
+
+
+      <tr height="50">
       <td><b>User Contact no:</b></td>
       <td><?php echo htmlentities($row['contactNo']); ?></td>
     </tr>
-    <tr height="50">
+    
+
+
+        <tr height="50">
       <td><b>Address:</b></td>
       <td><?php echo htmlentities($row['address']); ?></td>
     </tr>
-    <tr height="50">
+
+
+
+        <tr height="50">
       <td><b>State:</b></td>
       <td><?php echo htmlentities($row['State']); ?></td>
     </tr>
-    <tr height="50">
+
+
+        <tr height="50">
       <td><b>Country:</b></td>
       <td><?php echo htmlentities($row['country']); ?></td>
     </tr>
-    <tr height="50">
+
+
+        <tr height="50">
       <td><b>Pincode:</b></td>
       <td><?php echo htmlentities($row['pincode']); ?></td>
-    </tr>
-    <tr height="50">
+    </tr>  
+
+
+        <tr height="50">
       <td><b>Last Updation:</b></td>
       <td><?php echo htmlentities($row['updationDate']); ?></td>
     </tr>
-    <tr height="50">
+     <tr height="50">
       <td><b>Status:</b></td>
       <td><?php if($row['status']==1)
       { echo "Active";
@@ -85,14 +106,23 @@ while($row=mysqli_fetch_array($ret1))
 }
         ?></td>
     </tr>
+    
     <tr>
+  
       <td colspan="2">   
       <input name="Submit2" type="submit" class="txtbox4" value="Close this window " onClick="return f2();" style="cursor: pointer;"  /></td>
     </tr>
-    <?php } ?>
+   
+    <?php } 
+
+ 
+    ?>
+ 
 </table>
  </form>
 </div>
+
 </body>
 </html>
-<?php } ?>
+
+     <?php } ?>
--- categoryadmin/updatecomplaints.php
+++ categoryadmin/updatecomplaints.php
@@ -1,34 +1,63 @@
 <?php
 session_start();
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-  { 
-header('location:index.php');
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+if (strlen($_SESSION['login']) == 0) { 
+    header('location:index.php');
+} else {
+    if (isset($_POST['update'])) {
+        $complaintnumber = $_GET['cid'];
+        $status = $_POST['status'];
+        $remark = $_POST['remark'];
+
+        // Insert complaint remark
+        $query = mysqli_query($bd, "insert into complaintremark(complaintNumber,status,remark) values('$complaintnumber','$status','$remark')");
+
+        // Update complaint status
+        $sql = mysqli_query($bd, "update tblcomplaints set status='$status' where complaintNumber='$complaintnumber'");
+
+        // Get user email and name
+       $complaintInfo = mysqli_query($bd, "SELECT u.fullName, u.userEmail FROM tblcomplaints c JOIN users u ON c.userId = u.id WHERE c.complaintNumber = '$complaintnumber'");
+        $row = mysqli_fetch_assoc($complaintInfo);
+        $userName = $row['fullName'];
+        $userEmail = $row['userEmail'];
+
+        echo "<script>alert('Complaint details updated successfully');</script>";
+
+        $email = new \SendGrid\Mail\Mail(); 
+        $email->setFrom("Darusocomplaint@outlook.com", "Daruso");
+        $email->setSubject("Complaints");
+        $email->addTo($userEmail, $userName);
+        $email->addContent("text/plain", "please visit now Daruso Compliant Management system to track the status of your compliant");
+        $email->addContent(
+            "text/html", "<strong>please visit now Daruso Compliant Management system to track the status of your compliant</strong>"
+        );
+        $sendgrid = new \SendGrid("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");
+        try {
+            $response = $sendgrid->send($email);
+            print $response->statusCode() . "\n";
+            print_r($response->headers());
+            print $response->body() . "\n";
+        } catch (Exception $e) {
+            echo 'Caught exception: '. $e->getMessage() ."\n";
+        }
+    }
 }
-else {
-  if(isset($_POST['update']))
-  {
-$complaintnumber=$_GET['cid'];
-$status=$_POST['status'];
-$remark=$_POST['remark'];
-$query=mysqli_query($bd, "insert into complaintremark(complaintNumber,status,remark) values('$complaintnumber','$status','$remark')");
-$sql=mysqli_query($bd, "update tblcomplaints set status='$status' where complaintNumber='$complaintnumber'");
+?>
 
-echo "<script>alert('Complaint details updated successfully');</script>";
-
-  }
-
- ?>
 <script language="javascript" type="text/javascript">
-function f2()
-{
-window.close();
-}ser
-function f3()
-{
-window.print(); 
+function f2() {
+    window.close();
+}
+
+function f3() {
+    window.print(); 
 }
 </script>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -43,8 +72,8 @@ window.print();
  <form name="updateticket" id="updatecomplaint" method="post"> 
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr>
-      <td  >&nbsp;</td>
-      <td >&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
     <tr height="50">
       <td><b>Complaint Number</b></td>
@@ -52,44 +81,32 @@ window.print();
     </tr>
     <tr height="50">
       <td><b>Status</b></td>
-      <td><select name="status" required="required">
-      <option value="">Select Status</option>
-      <option value="in process">In Process</option>
-    <option value="closed">Closed</option>
-        
-      </select></td>
+      <td>
+        <select name="status" required="required">
+          <option value="">Select Status</option>
+          <option value="in process">In Process</option>
+          <option value="closed">Closed</option>
+        </select>
+      </td>
     </tr>
-
-
-      <tr height="50">
+    <tr height="50">
       <td><b>Remark</b></td>
       <td><textarea name="remark" cols="50" rows="10" required="required"></textarea></td>
     </tr>
-    
-
-
-        <tr height="50">
+    <tr height="50">
       <td>&nbsp;</td>
       <td><input type="submit" name="update" value="Submit"></td>
     </tr>
-
-
-
-       <tr><td colspan="2">&nbsp;</td></tr>
-    
+    <tr><td colspan="2">&nbsp;</td></tr>
     <tr>
-  <td></td>
-      <td >   
-      <input name="Submit2" type="submit" class="txtbox4" value="Close this window " onClick="return f2();" style="cursor: pointer;"  /></td>
+      <td></td>
+      <td>   
+        <input name="Submit2" type="submit" class="txtbox4" value="Close this window" onClick="return f2();" style="cursor: pointer;"  />
+      </td>
     </tr>
-   
-
- 
 </table>
  </form>
 </div>
 
 </body>
 </html>
-
-     <?php } ?>
